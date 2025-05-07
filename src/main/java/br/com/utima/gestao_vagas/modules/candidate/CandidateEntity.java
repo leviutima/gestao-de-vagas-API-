08d282bb71f192d9921c0de1b448ebfb13a46163
leviutima <levi.utima@gmail.com>
2025-05-07 00:26:28 -0300

    first commit

---
+++ src/main/java/br/com/utima/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,15 @@
+package br.com.utima.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+    private String userName;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+
+}
