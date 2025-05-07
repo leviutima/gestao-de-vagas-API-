99d1bad2ce2c3a5d1d7ec10c55e465fb25d168ce
leviutima <levi.utima@gmail.com>
2025-05-07 00:43:45 -0300

    feat: implement validate

--- src/main/java/br/com/utima/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/utima/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,12 +2,23 @@ package br.com.utima.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+@Data
 public class CandidateEntity {
 
     private UUID id;
     private String name;
-    private String userName;
+
+    private String username;
+
+    @Email(message= "O campo email deve conter um email válido")
     private String email;
+
+    @Length(min = 6)
     private String password;
     private String description;
     private String curriculum;
--- src/main/java/br/com/utima/gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/utima/gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,16 +1,21 @@
 package br.com.utima.gestao_vagas.modules.candidate.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.utima.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
     
-    @PostMapping("/")
-    public void create() {
-
+    @PostMapping()
+    public void create( @Valid @RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
     }
 
 }
